596fd72058f40fb05c448fa0ceb2b8d1fac00ba9
Kleber2018 <42260292+Kleber2018@users.noreply.github.com>
2021-10-14 21:11:59 -0300

    up

--- src/app/dash/dash/dash.component.ts
+++ src/app/dash/dash/dash.component.ts
@@ -5,7 +5,6 @@ import { Usuario } from 'src/app/usuario/usuario.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { SidenavItemFactory } from 'src/app/shared/factory/sidenav-item.factory';
-import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'app-dash',
@@ -24,21 +23,11 @@ export class DashComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
-    private formBuilder: FormBuilder,
-    private authenticationService: AuthenticationService
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
-
-    this.usuario = sessionStorage.getItem('usuario')
-    ? JSON.parse(sessionStorage.getItem('usuario'))
-    : null;
-
-    if (this.usuario) {
       this.sidenavItems = SidenavItemFactory.buildSidenav();
-    } else {
-      this.authenticationService.logout();
-    }
   }
 
 }
